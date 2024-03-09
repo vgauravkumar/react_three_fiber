3c19c382aaed64bc7075fbfc7a19c7b7dc61ca5d
vgauravkumarbackend <gauravverma@careercarve.com>
2024-03-09 13:28:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { MeshStandardMaterial } from 'three'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Canvas>
+      <directionalLight position={[0, 0, 2]}/>
+      <mesh position={[1, 0, 2]}>
+
+        {/* Default is 1x1x1 box */}
+        <boxGeometry args={[1, 1, 1]}/>
+
+        {/* Mesh basic material needed light to be viewed */}
+        <meshStandardMaterial color="orange" />
+        {/* <meshBasicMaterial/> */}
+      </mesh>
+
+      <mesh position={[-1, 0, -2]}>
+
+        {/* Default is 1x1x1 box */}
+        <boxGeometry args={[1, 1, 1]}/>
+
+        {/* Mesh basic material needed light to be viewed */}
+        <meshStandardMaterial color="orange" />
+        {/* <meshBasicMaterial/> */}
+      </mesh>
+      
+    </Canvas>
+  )
+}
+
+export default App
